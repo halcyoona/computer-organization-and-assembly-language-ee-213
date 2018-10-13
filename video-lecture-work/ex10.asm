5a4df66038591b7047b9278c9affadf3150e9abc
halcyoona <halcyoona@gmail.com>
2018-10-13 20:09:43 +0500

    g

---
+++ video-lecture-work/ex10.asm
@@ -0,0 +1,27 @@
+;a program to add ten numbers without  using seperate counter 
+
+[org 0x0100]
+
+
+	mov bx, 0
+	mov ax, 0
+
+l1:	add ax, [num1+bx]
+	add	bx, 2
+	cmp bx, 20
+	jne l1
+
+	mov [total], ax
+
+
+	mov ax, 0x4c00
+	int 0x21
+
+
+
+
+
+
+
+num1:	dw	10, 20, 30, 40, 50, 60, 70, 80, 90, 100
+total:	dw	0
